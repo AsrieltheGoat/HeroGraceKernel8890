822a1fbaa2d43a60ea4e489f5f9e74d8e2737c09
boloaimer <q2591187844@gmail.com>
2025-09-19 15:54:11 +0800

    cronos.sh: Change kernel name and version

--- cronos.sh
+++ cronos.sh
@@ -38,8 +38,8 @@ CR_KERNEL=$CR_DIR/arch/arm64/boot/Image
 # Compiled dtb by dtbtool
 CR_DTB=$CR_DIR/arch/arm64/boot/dtb.img
 # Kernel Name and Version
-CR_VERSION=V8.0
-CR_NAME=CronosKernel
+CR_VERSION=V1.0
+CR_NAME=HeroGraceKernel
 # Thread count
 CR_JOBS=$(nproc --all)
 # Target Android version
